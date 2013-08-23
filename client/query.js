7286067db9e7c8a27a8c632665c161f4f40a0d66
Scott Heckel <scott.heckel@gmail.com>
2013-08-23 18:27:49 -0500

    Adding Each to Query

--- client/query.js
+++ client/query.js
@@ -1,35 +1,36 @@
-(function() {
-	var $ = function(selector) {
+(function(exports, doc) {
+	var query = function(selector) {
 		var els = null;
 		if(typeof selector === "string") {
-			els = selector[0] == '#' ? [document.getElementById(selector.substring(1))] : document.getElementsByClassName(selector);
+			els = selector[0] == '#' ? [doc.getElementById(selector.substring(1))] : doc.getElementsByClassName(selector);
 		} else if(!!selector) {
 			els = [selector];
 		}
 		return {
 			on: function(eventName, handler) {
-				var index = 0,
-					el;
-				for(;index < els.length;index++) {
-					el = els[index];
+				query.each(els, function(el) {
 					if(el.addEventListener) {
 						el.addEventListener(eventName, handler, false);
 					} else {
 						el.attachEvent('on' + eventName, handler);
 					}
-				}
+				});
 			},
 		    attr: function(name, value) {
-		      var index = 0,
-		          el;
-		      for(;index<els.length;index++) {
-		        els[index].setAttribute(name, value);
-		      }
+		    	query.each(els, function(el) {
+		    		el.setAttribute(name, value);
+		    	});
 		    }
 		};
 	};
-	$.template = function(template, data) {
-      return template.replace(/\{\{(\w*)\}\}/g, function(m,key){return data.hasOwnProperty(key)?data[key]:"";});
-    };
-	window.$ = $;
-})();
+	query.template = function(template, data) {
+		return template.replace(/\{\{(\w*)\}\}/g, function(m,key){return data.hasOwnProperty(key)?data[key]:"";});
+	};
+	query.each = function(array, callback) {
+		var index = 0;
+		for(;index<array.length;index++) {
+			callback(array[index], index);
+		}
+	};
+	exports.$ = query;
+})(window, document);
